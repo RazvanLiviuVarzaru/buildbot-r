d857d2061b53fd43fdcc8eb81b6a4f55b6638cde
Razvan-Liviu Varzaru <razvan@mariadb.org>
2024-11-21 17:21:38 +0200

    fix isort

--- utils.py
+++ utils.py
@@ -10,25 +10,16 @@ from buildbot.process.properties import Properties, Property
 from buildbot.process.remotecommand import RemoteCommand
 from buildbot.process.results import FAILURE
 from buildbot.steps.mtrlogobserver import MTR, MtrLogObserver
-from buildbot.steps.shell import Compile, SetPropertyFromCommand, ShellCommand, Test
+from buildbot.steps.shell import (Compile, SetPropertyFromCommand,
+                                  ShellCommand, Test)
 from buildbot.steps.source.github import GitHub
 from pyzabbix import ZabbixAPI
 from twisted.internet import defer
 
-from constants import (
-    DEVELOPMENT_BRANCH,
-    MTR_ENV,
-    builders_autobake,
-    builders_big,
-    builders_eco,
-    builders_galera_mtr,
-    builders_install,
-    builders_s3_mtr,
-    builders_upgrade,
-    os_info,
-    releaseBranches,
-    savedPackageBranches,
-)
+from constants import (DEVELOPMENT_BRANCH, MTR_ENV, builders_autobake,
+                       builders_big, builders_eco, builders_galera_mtr,
+                       builders_install, builders_s3_mtr, builders_upgrade,
+                       os_info, releaseBranches, savedPackageBranches)
 
 private_config = {"private": {}}
 exec(open("/srv/buildbot/master/master-private.cfg").read(), private_config, {})
